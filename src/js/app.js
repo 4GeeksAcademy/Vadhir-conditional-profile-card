dd4d89e0c96e4d9617ca433a0d66595c5e82b912
Vadhir <lomuner@gmail.com>
2024-10-04 06:29:04 +0000

    Second Commit

--- src/js/app.js
+++ src/js/app.js
@@ -32,15 +32,25 @@ function render(variables = {}) {
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name}  ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <img src="${variables.avatarURL ||
+            "https://via.placeholder.com/150"}" class="photo" />
+          <h1>${variables.name || ""} ${variables.lastName || ""}</h1>
+          <h2>${variables.role || ""}</h2>
+          <h3>${variables.city || ""}, ${variables.country || ""}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a>${variables.twitter}</li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a>${variables.github}</li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a>${variables.linkedin}</li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a>${variables.instagram}</li>
+            <li><a href="${
+              variables.twitter
+            }"><i class="fab fa-twitter"></i></a>
+            </li>
+            <li><a href="${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="${
+              variables.linkedin
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="${
+              variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
